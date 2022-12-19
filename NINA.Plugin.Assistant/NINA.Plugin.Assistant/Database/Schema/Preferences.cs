acc32aeef0cfdae84dd3aa7aa3cb9ec9f733cf57
Tom Palmer <palmer.tom@gmail.com>
2022-12-19 12:39:42 -0500

    spikes

---
+++ NINA.Plugin.Assistant/NINA.Plugin.Assistant/Database/Schema/Preferences.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Assistant.NINAPlugin.Database.Schema {
+
+    public class Preferences {
+
+        [Key]
+        public int id { get; set; }
+
+        // TODO: not clear how we want to organize this.  Maybe each row is a key/value where value is a string ...
+    }
+
+    internal class PreferencesConfiguration : EntityTypeConfiguration<Preferences> {
+
+        public PreferencesConfiguration() {
+            ToTable("dbo.preferences");
+            HasKey(x => x.id);
+        }
+
+    }
+
+}
