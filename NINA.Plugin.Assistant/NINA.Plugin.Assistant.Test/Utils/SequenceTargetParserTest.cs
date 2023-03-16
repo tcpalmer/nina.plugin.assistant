b04c5cb5c355a65c5f7fa324c09accd751f6e117
Tom Palmer <palmer.tom@gmail.com>
2023-03-16 07:54:17 -0400

    target import from sequence targets

---
+++ NINA.Plugin.Assistant/NINA.Plugin.Assistant.Test/Utils/SequenceTargetParserTest.cs
@@ -0,0 +1,50 @@
+﻿using Assistant.NINAPlugin.Util;
+using FluentAssertions;
+using NINA.Astrometry;
+using NUnit.Framework;
+using System.IO;
+
+namespace NINA.Plugin.Assistant.Test.Util {
+
+    [TestFixture]
+    public class SequenceTargetParserTest {
+
+        [Test]
+        public void TestParse() {
+            string tmpFileName = Path.GetTempFileName();
+            File.WriteAllText(tmpFileName, J1);
+
+            SequenceTarget st = SequenceTargetParser.GetSequenceTarget(tmpFileName);
+            st.TargetName.Should().Be("M 42");
+            st.Rotation.Should().BeApproximately(123.4, 0.0001);
+
+            Coordinates c = st.GetCoordinates();
+            c.RAString.Should().Be("05:35:17");
+            c.DecString.Should().Be("-05° 23' 28\"");
+        }
+
+        private string J1 = @"
+{
+  ""$id"": ""1"",
+  ""$type"": ""NINA.Sequencer.Container.DeepSkyObjectContainer, NINA.Sequencer"",
+  ""Target"": {
+    ""$id"": ""2"",
+    ""$type"": ""NINA.Astrometry.InputTarget, NINA.Astrometry"",
+    ""Expanded"": true,
+    ""TargetName"": ""M 42"",
+    ""Rotation"": 123.4,
+    ""InputCoordinates"": {
+      ""$id"": ""3"",
+      ""$type"": ""NINA.Astrometry.InputCoordinates, NINA.Astrometry"",
+      ""RAHours"": 5,
+      ""RAMinutes"": 35,
+      ""RASeconds"": 17.0,
+      ""NegativeDec"": true,
+      ""DecDegrees"": -5,
+      ""DecMinutes"": 23,
+      ""DecSeconds"": 28.0
+    }
+  },
+";
+    }
+}
---
+++ NINA.Plugin.Assistant/NINA.Plugin.Assistant/Utils/SequenceTargetParser.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using NINA.Astrometry;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Assistant.NINAPlugin.Util {
+
+    /// <summary>
+    /// This is really ugly but unfortunately you can't just deserialize this type of serialized object directly since
+    /// you get a NPE.  My guess is that NINA does it only in the context of having a real lat/long which makes it possible
+    /// to construct a NINA.Astrometry.InputCoordinates.
+    /// </summary>
+    public class SequenceTargetParser {
+
+        public static SequenceTarget GetSequenceTarget(string pathToFile) {
+            string json = GetJson(pathToFile);
+            return JsonConvert.DeserializeObject<SequenceTarget>(json);
+        }
+
+        private static string GetJson(string pathToFile) {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{");
+            bool inObject = false;
+            foreach (string line in File.ReadAllLines(pathToFile)) {
+
+                if (line.Contains("TargetName") || line.Contains("Rotation")) {
+                    sb.Append(line);
+                }
+
+                if (!inObject && line.Contains("InputCoordinates")) {
+                    inObject = true;
+                    continue;
+                }
+
+                if (inObject && line.Contains("}")) {
+                    sb.Append("}");
+                    return sb.ToString();
+                }
+
+                if (inObject && !line.Contains("$")) {
+                    sb.Append(line);
+                }
+            }
+
+            throw new Exception($"failed to parse sequence target json from file (key lines not found): {pathToFile}");
+        }
+
+        private SequenceTargetParser() { }
+    }
+
+    public class SequenceTarget {
+        int RAHours { get; set; }
+        int RAMinutes { get; set; }
+        double RASeconds { get; set; }
+        bool NegativeDec { get; set; }
+        int DecDegrees { get; set; }
+        int DecMinutes { get; set; }
+        double DecSeconds { get; set; }
+
+        public string TargetName { get; set; }
+        public double Rotation { get; set; }
+
+        public SequenceTarget(int raHours, int raMinutes, double raSeconds,
+            bool negativeDec, int decDegrees, int decMinutes, double decSeconds,
+            string targetName, double rotation) {
+
+            RAHours = raHours;
+            RAMinutes = raMinutes;
+            RASeconds = raSeconds;
+            NegativeDec = negativeDec;
+            DecDegrees = decDegrees;
+            DecMinutes = decMinutes;
+            DecSeconds = decSeconds;
+            TargetName = targetName;
+            Rotation = rotation;
+        }
+
+        public Coordinates GetCoordinates() {
+            string hms = string.Format("{0:00}:{1:00}:{2:00}", RAHours, RAMinutes, RASeconds);
+            string dms = string.Format("{0:00}:{1:00}:{2:00}", DecDegrees, DecMinutes, DecSeconds);
+            return new Coordinates(AstroUtil.HMSToDegrees(hms), AstroUtil.DMSToDegrees(dms), Epoch.J2000, Coordinates.RAType.Degrees);
+        }
+
+    }
+
+}
